511a025bd86b5116fa956eea460993f579c8e7cb
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-03 21:53:27 +0530

    more block code

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-
+import { shared, env } from "@appblocks/node-sdk";
 
 const sample_otp_verification_fn = async (req, res) => {
-
+  const { prisma, getBody, sendResponse } = await shared.getShared();
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    sendResponse(res, 200, { success: true, msg: "Health check success" });
+    return;
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello sample_otp_verification_fn`}))
-  res.end()
-  
-}
+  const { email, email_verification_code } = await getBody(req);
+  try {
+    const userData = await prisma.users.findFirst({ where: { email } });
 
-export default sample_otp_verification_fn
+    console.log("userData:", userData);
+    if (!userData) {
+      sendResponse(res, 404, {
+        err: true,
+        msg: "email not found",
+        data: {},
+      });
+      return;
+    }
+
+    if (userData.email_verification_code != email_verification_code) {
+      sendResponse(res, 400, {
+        err: true,
+        msg: "Wrong verification code",
+        data: {},
+      });
+      return;
+    }
+
+    sendResponse(res, 200, {
+      err: false,
+      msg: "otp verified",
+      data: {
+        user_name: userData.user_name,
+        email: userData.email,
+      },
+    });
+    return;
+  } catch (error) {
+    sendResponse(res, 500, {
+      err: true,
+      msg: "server error",
+      data: { message: error.message },
+    });
+    return;
+  }
+};
+
+export default sample_otp_verification_fn;
